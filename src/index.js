cdca94719a72770b55e2f26be91fd8f565b99166
Sohail Hasware <kd3180@hotmail.com>
2019-03-11 18:22:54 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import {bro} from './bro'
+import './styles/main.scss'
+
+document.querySelector('h1').textContent = bro(`How is it going`)
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import {bro} from './lib/bro'
+import '../styles/main.scss'
+
+
+document.querySelector('h3').textContent = bro(`How is it going`)
