648657f94e5e2be2672f1f2b1a701e5b1b7695e8
Sohail Hasware <kd3180@hotmail.com>
2019-03-11 22:22:30 +0100

    use id instead of html tag with getElementById

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
 import {bro} from './lib/bro'
 import '../styles/main.scss'
 
-
-document.querySelector('h1').textContent = bro(`How is it going`)
+document.getElementById('myID').textContent = bro(`How is it going`)
