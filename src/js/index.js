23cac46aceca08789334afd14c157911df34ef4c
Sohail Hasware <kd3180@hotmail.com>
2019-03-11 21:53:08 +0100

    changes to index.js on page load

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,6 @@ import {bro} from './lib/bro'
 import '../styles/main.scss'
 
 
-document.querySelector('h3').textContent = bro(`How is it going`)
+window.onload = function () {
+    document.querySelector('#myId').textContent = bro(`How is it going`)
+}
